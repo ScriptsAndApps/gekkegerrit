a2c49e8f255b278f58e91f53c402cd71cc32eff8
ScriptsAndApps <99158605+ScriptsAndApps@users.noreply.github.com>
2023-04-11 20:02:37 +0200

    Add files via upload

---
+++ startserver.bat
@@ -0,0 +1,30 @@
+:start
+
+
+cs2.exe -console -insecure -usercon +mapgroup mg_active -maxplayers_override 24 +map de_dust2 +game_mode 1 +game_type 0 +mp_autoteambalance 0 +map de_dust2 +sv_cheats 1 +mp_warmup_end 1 +mp_buy_anywhere 1 +bot_difficulty 0 +sv_bots_get_easier_each_win 1 +sv_stressbots true +bot_allow_pistols false +bot_allow_grenades false +bot_allow_machine_guns false +bot_allow_rifles false +bot_allow_shotguns true +bot_allow_rogues false +bot_allow_snipers false +bot_allow_sub_machine_guns false +ammo_grenade_limit_total 5 +sv_infinite_ammo 1 +mp_startmoney 9999 +mp_buytime 9999
+
+exit 
+
+> bot_difficulty 0
+> sv_bots_get_easier_each_win 1
+> sv_stressbots true
+> bot_allow_pistols false
+> bot_allow_grenades false
+> bot_allow_machine_guns false
+> bot_allow_rifles false
+> bot_allow_shotguns true
+> bot_allow_rogues false
+> bot_allow_snipers false
+> bot_allow_sub_machine_guns false
+
+
+rem cs2.exe -game csgo -console -usercon -maxplayers_override 24 +game_type 1 +game_mode 2 +mapgroup mg_active +map de_dust2 -nobots -authkey BDABF8498FF95FB1A21573047FE48D04
+
+rem -tickrate 128 
+rem  +mp_maxmoney 99999 +mp_startmoney 99999 +mp_buytime 9999 +mp_buy_anywhere 1 +mp_warmup_end 1 
+
+rem +mp_limitteams 0 +mp_freezetime 0 + mp_roundtime 60 +mp_roundtime_defuse 60 +mp_roundtime_hostage 60 +mp_maxmoney 99999 +mp_startmoney 99999 +mp_buytime 9999 +mp_buy_anywhere 1 +ammo_grenade_limit_total 5 +sv_infinite_ammo 1 +
+
+rem +map de_dust2 
+
+rem goto start:
